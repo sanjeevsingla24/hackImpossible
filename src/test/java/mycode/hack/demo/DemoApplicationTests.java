534e59efdbc781260428613e094a2b7113804708
Sanjeev Singla <sanjeev.singla@trantorinc.com>
2024-10-07 15:42:16 -0400

    Initial code

---
+++ src/test/java/mycode/hack/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mycode.hack.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
