b497dbe620e49e06c01082b2aa751ca22aae362c
Ankur Bansal <ankurbansal@Ankurs-MacBook-Pro.local>
2024-10-23 08:49:45 -0500

    vulnerable log4j integration

--- src/main/java/mycode/hack/demo/DemoApplication.java
+++ src/main/java/mycode/hack/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package mycode.hack.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class DemoApplication {
+	private static final Logger logger = LogManager.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		logger.info("Hello World!");
 	}
 
 }
